af13f3bd39c7dd9028132eeb49033c245f2fb2bd
Matt McCutchen <matt@mattmccutchen.net>
2016-12-03 09:57:37 -0500

    Initial commit of reproduction of concurrency problem with babrahams:transactions.

---
+++ concurrent.js
@@ -0,0 +1,55 @@
+tx.requireUser = false;
+
+var Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient) {
+    Router.route("/", function() {
+        this.render("main");
+    });
+
+    Template.main.helpers({
+        allDocs: function() {
+            return Documents.find().fetch();
+        }
+    });
+
+    Template.main.events({
+        "click #run_concurrent_txns": function() {
+            document.getElementById("fa").contentWindow.insertDocuments();
+            document.getElementById("fb").contentWindow.insertDocuments();
+        },
+        "click #undo": function() {
+            tx.undo();
+        }
+    });
+
+    var client_id;  // used only on client
+    Router.route("/:client_id", function() {
+        this.render("one_client", {data: {client_id: this.params.client_id}});
+        client_id = this.params.client_id;
+    });
+
+    Template.one_client.events({
+        "click #run_txn": function() {
+            window.insertDocuments();
+        }
+    });
+
+    window.insertDocuments = function() {
+        Meteor.call("insertDocuments", client_id);
+    };
+}
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        insertDocuments: function(client_id) {
+            tx.start();
+            var pfx = Random.id();
+            for (var i = 0; i < 10; i++) {
+                var id = client_id + "_" + pfx + "_" + i;
+                Documents.insert({_id: id}, {tx: true});
+            }
+            tx.commit();
+        }
+    });
+}
